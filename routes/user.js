04d3209f375bc1768437e9cd635262742c421c2f
Asif R Choudhury <choudhury.asif@gmail.com>
2013-09-16 16:55:41 -0500

    User Schema functional and passes preliminary test

--- routes/user.js
+++ routes/user.js
@@ -8,14 +8,45 @@
  * and router interface to Express
  *
  * Instantiated with a constructor,
- *  - takes mongoose connection as parameter
+ *  - takes mongoose as parameter
 */
-module.exports = function(db) {
-	this.dbb = db;
-	this.connect = function(me) {
-		return function(cb) {
-			me.dbb.once('open', cb);
-		}
-	}(this);
+
+
+//Export Constructor
+module.exports = function(mongoose) {
+
+	//Mongoose Connection to MongoDB
+	var db = mongoose.connection;
+	
+	//Connect to MongoDB
+	this.connect = function(cb) {
+			db.once('open', cb);
+	};
+	
+	//User Schema
+	var schema = new mongoose.Schema({
+		fbid: String,
+		firstName: String,
+		lastName: String,
+		displayName: String,
+		department: String,
+		email: String,
+		password: String,
+		phone: String,
+		address: String,
+		city: String,
+		stateProv: String,
+		zip: String,
+		country: String,
+		profilePic: String,
+		TS: Date
+	});
+	
+	//User Model
+	var User = mongoose.model('User', schema);
+	
+	//Export Model
+	this.model = User;
+	
 	return this;
 };
--- test/user.create.js
+++ test/user.create.js
@@ -10,10 +10,25 @@ var assert = require("assert")
 //Connect to MongoDB
 var mongoUrl = "mongodb://localhost/buet73";
 mongoose.connect(mongoUrl);
-var user = require("../routes/user")(mongoose.connection);
+var user = require("../routes/user")(mongoose);
 
 describe("User Module Mongoose", function() {
+	
 	it("should connect to mongo", function(done) {
 		user.connect(done);
+	});
+	
+	describe("User Schema & Model", function() {
+		
+		it("should create a User object from the User model", function() {
+			var bob = {
+				fbid: "1234",
+				firstName: "Bob",
+				lastName: "Johnson"
+			};
+			var bobby = new user.model(bob);
+			assert.equal(bob.firstName, bobby.firstName);
+		});
+		
 	});
 });
