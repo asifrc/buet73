b80b2f478312d5956baaa9e244583cd7b0ac3936
Asif R Choudhury <choudhury.asif@gmail.com>
2014-01-08 22:25:10 -0600

    Added tests to user.register for optional and required fields with pass and fail conditions

--- test/user.js
+++ test/user.js
@@ -16,33 +16,57 @@ var db = mongoose.connection;
 //Require User Module, passing mongoose
 var user = require("../routes/user")(mongoose);
 
+var reqFields = [
+	'firstName',
+	'lastName',
+	'displayName',
+	'department',
+	'email',
+	'password',
+	'country'
+];
+var optFields = [
+	'fbid',
+	'phone',
+	'address',
+	'city',
+	'stateProv',
+	'zip'
+];
+
+
+
 describe("MongoDB Connection", function() {
 	it("should connect to mongo", function(done) {
 		db.once('open', done);
 	});
 });
 
+var newBob = function() {
+	return {
+		fbid: "1234",
+		firstName: "Robert",
+		lastName: "Johnson",
+		displayName: "Bobby Johnson",
+		department: "Electrical Engineering",
+		email: "rjohnson@asifchoudhury.com",
+		password: "unhashedpassword",
+		cpassword: "unhashedpassword",
+		phone: "1 123 1234",
+		address: "2828 82nd St",
+		city: "Rapid City",
+		stateProv: "South Dakota",
+		zip: "57702",
+		country: "United States",
+	};
+};
+
 describe("User Module", function() {
 	describe("User Model", function() {
 		var bob, bobby;
 		
 		beforeEach(function() {
-			bob = {
-				fbid: "1234",
-				firstName: "Robert",
-				lastName: "Johnson",
-				displayName: "Bobby Johnson",
-				department: "Electrical Engineering",
-				email: "rjohnson@asifchoudhury.com",
-				password: "unhashedpassword",
-				cpassword: "unhashedpassword",
-				phone: "1 123 1234",
-				address: "2828 82nd St",
-				city: "Rapid City",
-				stateProv: "South Dakota",
-				zip: "57702",
-				country: "United States",
-			};
+			bob = newBob();
 		});
 		
 		afterEach(function() {
@@ -58,15 +82,37 @@ describe("User Module", function() {
 			
 		});
 		describe("Registration", function() {
-			var reqFields = [
-				'firstName',
-				'lastName',
-				'displayName',
-				'department',
-				'email',
-				'password',
-				'country'
-			];
+			
+			beforeEach(function() {
+				bob = newBob();
+			});
+			
+			it("should return successful when there are no errors", function(done) {
+				user.register(bob, function(resp) {
+					(resp.error == null).should.be.ok;
+					done();
+				});
+			});
+			
+			describe("Required Fields", function() {
+				for (var i=0; i<reqFields.length; i++)
+				{
+					var field = reqFields[i];
+					it("should save "+field, function(done) {
+						bob[field] = "TestValue";
+						user.register(bob, function(resp) {
+							(resp.error == null).should.be.ok;
+							var obj = {};
+							obj[field] = "TestValue"
+							user.model.count(obj, function(err, count) {
+								count.should.equal(1);
+								done();
+							});
+						});
+					});
+				}
+			});
+			
 			describe("Missing Required Field", function() {
 				for (var i=0; i<reqFields.length; i++)
 				{
@@ -80,6 +126,57 @@ describe("User Module", function() {
 					});
 				}
 			});
+			
+			/*
+			describe("Empty String in Required Field", function() {
+				for (var i=0; i<reqFields.length; i++)
+				{
+					var field = reqFields[i];
+					it("should return an error when "+field+" is missing", function(done) {
+						bob = newBob();
+						bob[field] = "";
+						user.register(bob, function(resp) {
+							resp.error.should.equal("Bad request: "+field+" field is missing");
+							done();
+						});
+					});
+				}
+			});
+			*/
+			
+			describe("Optional Fields", function() {
+				for (var i=0; i<optFields.length; i++)
+				{
+					var field = optFields[i];
+					it("should save "+field, function(done) {
+						bob[field] = "TestValue";
+						user.register(bob, function(resp) {
+							(resp.error == null).should.be.ok;
+							var obj = {};
+							obj[field] = "TestValue"
+							user.model.count(obj, function(err, count) {
+								count.should.equal(1);
+								done();
+							});
+						});
+					});
+				}
+			});
+			
+			describe("Missing Optional Field", function() {
+				for (var i=0; i<reqFields.length; i++)
+				{
+					var field = reqFields[i];
+					it("should still save successfully when "+field+" is missing", function(done) {
+						delete bob[field];
+						user.register(bob, function(resp) {
+							resp.error.should.equal("Bad request: "+field+" field is missing");
+							done();
+						});
+					});
+				}
+			});
+			
 			describe("Invalid Password", function() {
 				it("should return an error when confirmation field is missing", function(done) {
 					delete bob.cpassword;
@@ -105,27 +202,9 @@ describe("User Module", function() {
 				});
 			});
 			
-			describe("No Calblaback", function() {
-				it("should not throw any errors)", function() {
+			describe("No Callback", function() {
+				it("should not throw any errors", function() {
 					(user.register(bob) || true).should.be.ok;
-				});
-			});
-			
-			it("should return successful when there are no errors", function(done) {
-				user.register(bob, function(resp) {
-					(resp.error == null).should.be.ok;
-					done();
-				});
-			});
-			
-			it("should save optional fields", function(done) {
-				bob.city = "Rapid City";
-				user.register(bob, function(resp) {
-					(resp.error == null).should.be.ok;
-					user.model.count({ city: "Rapid City" }, function(err, count) {
-						count.should.equal(1);
-						done();
-					});
 				});
 			});
 			
