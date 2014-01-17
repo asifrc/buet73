699628a039fa43635c9acce0b3a1ca3b122a9ab3
Asif R Choudhury <choudhury.asif@gmail.com>
2014-01-17 03:30:30 -0600

    User.register checks for required fields (passes 7/7)

--- models/User.js
+++ models/User.js
@@ -1,1 +1,69 @@
-//Bismillahvar reqFields = [	'firstName',	'lastName',	'displayName',	'department',	'email',	'password',	'country'];exports.reqFields = reqFields;var optFields = [	'fbid',	'phone',	'address',	'city',	'stateProv',	'zip'];exports.optFields = optFields;var allFields = reqFields.concat(optFields);exports.allFields = allFields;UserModel = function(obj) {	var self = this;	obj = (typeof obj === "object") ? obj : {};	for (var i=0; i<allFields.length; i++)	{		var field = allFields[i];		self[field] = (typeof obj[field] === "undefined") ? null : obj[field];	}};exports.Model = UserModel;
+//Bismillah
+
+/**
+* User Model
+*/
+
+var reqFields = [
+	'firstName',
+	'lastName',
+	'displayName',
+	'department',
+	'email',
+	'password',
+	'country'
+];
+exports.reqFields = reqFields;
+var optFields = [
+	'fbid',
+	'phone',
+	'address',
+	'city',
+	'stateProv',
+	'zip'
+];
+exports.optFields = optFields;
+var allFields = reqFields.concat(optFields);
+exports.allFields = allFields;
+
+/**
+* Class: User object
+*/
+UserModel = function(obj) {
+	var self = this;
+	obj = (typeof obj === "object") ? obj : {};
+	for (var i=0; i<allFields.length; i++)
+	{
+		var field = allFields[i];
+		self[field] = (typeof obj[field] === "undefined") ? null : obj[field];
+	}
+};
+exports.Model = UserModel;
+
+
+var respond = function (callback, err, result) {
+	err = (typeof err === "undefined") ? null : err;
+	if (typeof callback === "function")
+	{
+		callback(err, result);
+	}
+	return { err: err, result: result };
+};
+
+/**
+* Create a User
+*/
+exports.register = function(user, cb) {
+	var err = null;
+	
+	for (var i=0; i<reqFields.length; i++)
+	{
+		var field = reqFields[i];
+		if (typeof user[field] === "undefined")
+		{
+			err = "Registration Error: "+field+" field is missing";
+			return respond(cb, err);
+		}
+	}
+	return respond(cb, err);
+};
--- test/models.User.js
+++ test/models.User.js
@@ -1,1 +1,67 @@
-//Bismillah/*** Tests for models/User.js*/var User = require('../models/User');var should = require("should")	, assert = require("assert");	var validUser = function() {	return {		fbid: "1234",		firstName: "Bob",		lastName: "Anderson",		displayName: "Bobby Anderson",		department: "Electrical Engineering",		email: "banderson@asifchoudhury.com",		password: "unhashedpassword",		phone: "1 123 1234",		address: "2828 82nd St",		city: "Rapid City",		stateProv: "South Dakota",		zip: "57702",		country: "United States",	};};describe("User Model", function() {	describe("UserModel", function() {		it("should create a new User object", function() {			var user = new User.Model();			(typeof user).should.equal("object");		});		for (var i=0; i<User.allFields.length; i++)		{			var field = User.allFields[i];			it("should contain a `"+field+"` property", function() {				var user = new User.Model();				(typeof user[field]).should.equal("object");			});			it("should set a value for `"+field+"` when present in parameter", function() {				var obj = {};				obj[field] = "Value";				var user = new User.Model(obj);				user[field].should.equal("Value");			});		}	});	describe("Register", function() {		describe("Validation", function() {			for (var i=0; i<User.reqFields.length; i++)			{				var field = User.reqFields[i];				it("should return an error when `"+field+"` is missing", function(done) {					var invalidUser = validUser();					delete invalidUser[field];					User.register(invalidUser, function(err, result) {						err.should.equal("Registration Error: "+field+" field is missing");						done();					});				});			}		});	});});
+//Bismillah
+
+/**
+* Tests for models/User.js
+*/
+
+var User = require('../models/User');
+
+var should = require("should")
+	, assert = require("assert");
+	
+var validUser = function() {
+	return {
+		fbid: "1234",
+		firstName: "Bob",
+		lastName: "Anderson",
+		displayName: "Bobby Anderson",
+		department: "Electrical Engineering",
+		email: "banderson@asifchoudhury.com",
+		password: "unhashedpassword",
+		phone: "1 123 1234",
+		address: "2828 82nd St",
+		city: "Rapid City",
+		stateProv: "South Dakota",
+		zip: "57702",
+		country: "United States",
+	};
+};
+
+describe("User Model", function() {
+	describe("UserModel", function() {
+		it("should create a new User object", function() {
+			var user = new User.Model();
+			(typeof user).should.equal("object");
+		});
+		for (var i=0; i<User.allFields.length; i++)
+		{
+			var field = User.allFields[i];
+			it("should contain a `"+field+"` property", function() {
+				var user = new User.Model();
+				(typeof user[field]).should.equal("object");
+			});
+			it("should set a value for `"+field+"` when present in parameter", function() {
+				var obj = {};
+				obj[field] = "Value";
+				var user = new User.Model(obj);
+				user[field].should.equal("Value");
+			});
+		}
+	});
+	describe("Register", function() {
+		describe("Validation", function() {
+			for (var i=0; i<User.reqFields.length; i++)
+			{
+				var field = User.reqFields[i];
+				it("should return an error when `"+field+"` is missing", function(done) {
+					var invalidUser = validUser();
+					delete invalidUser[field];
+					User.register(invalidUser, function(err, result) {
+						err.should.equal("Registration Error: "+field+" field is missing");
+						done();
+					});
+				});
+			}
+		});
+	});
+});
