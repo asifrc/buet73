b0e2c896e17eaad4cfc1034246709fa3eb125e57
Asif R Choudhury <choudhury.asif@gmail.com>
2014-01-26 13:38:53 -0600

    PostModel constructor accepts content and owner parameters(5/5 passing)

--- models/Post.js
+++ models/Post.js
@@ -4,7 +4,28 @@
 * Post Model
 */
 
-var PostModel = function() {
+var User = require('./User');
 
+var PostModel = function(content, owner) {
+	var self = this;
+	self.content = null;
+	self.owner = null;
+	
+	if (typeof content !== "undefined")
+	{
+		this.content = content;
+	}
+	if (typeof owner !== "undefined")
+	{
+		if (owner.constructor === User.Model)
+		{
+			self.owner = owner;
+		}
+		else if (typeof owner === "string" || typeof owner === "number")
+		{
+			self.owner = new User.Model();
+			self.owner.id(owner.toString());
+		}
+	}
 };
 exports.Model = PostModel;
--- test/models.Post.js
+++ test/models.Post.js
@@ -14,9 +14,38 @@ var should = require("should"),
 describe("Post Model", function() {
 	describe("PostModel Class", function() {
 		describe("Initialization", function() {
+			var content, owner, ownerId;
+			before(function() {
+				content = "This is a post.";
+				owner = new User.Model();
+				ownerId = "1234";
+				owner.id(ownerId);
+			});
 			it("should create a Post object", function() {
 				var post = new Post.Model();
 				post.should.be.an.instanceof(Post.Model);
+			});
+			it("should set the content property when passed", function() {
+				var post = new Post.Model(content);
+				post.content.should.equal(content);
+			});
+			it("should set the owner property when passed content and a User object", function() {
+				var post = new Post.Model(content, owner);
+				post.content.should.equal(content);
+				post.owner.should.be.an.instanceOf(User.Model);
+				post.owner.id().should.equal(ownerId);
+			});
+			it("should cast owner to a full User object when passed a string", function() {
+				var post = new Post.Model(content, ownerId);
+				post.content.should.equal(content);
+				post.owner.should.be.an.instanceOf(User.Model);
+				post.owner.id().should.equal(ownerId);
+			});
+			it("should cast owner to a full User object when passed a number", function() {
+				var post = new Post.Model(content, parseInt(ownerId));
+				post.content.should.equal(content);
+				post.owner.should.be.an.instanceOf(User.Model);
+				post.owner.id().should.equal(ownerId);
 			});
 		});
 	});
