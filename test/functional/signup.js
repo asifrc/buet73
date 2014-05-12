7f964bb35451389e16a8939630fb3074a7191861
Asif R Choudhury <choudhury.asif@gmail.com>
2014-05-11 21:58:28 -0500

    Wrote a passing functional test using CasperJS. Uses command  ./node_modules/casperjs/bin/casperjs test ./test/functional

---
+++ test/functional/signup.js
@@ -0,0 +1,15 @@
+//Bismillah
+
+// var casper = require('casper').create();
+
+casper.test.begin('Bismillah Test', function(test) {
+  casper.start('http://localhost:3000/');
+
+  casper.then(function() {
+    test.assertTitle("Class of '73", "Has the right title");
+  })
+
+  casper.run(function() {
+    casper.run();
+  });
+});
