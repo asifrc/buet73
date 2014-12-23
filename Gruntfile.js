76cc909518ac3c60050141fff9828662e0f3b9ca
Asif R Choudhury <choudhury.asif@gmail.com>
2014-12-23 00:03:51 -0600

    Added dummy mocha test and set up Grunt with Mocha and jsHint

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+//Bismillah
+var path = require('path');
+
+module.exports = function(grunt) {
+
+  log = function(err, stdout, stderr, cb) {
+    console.log(stdout);
+    cb();
+  };
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    simplemocha: {
+      options: {
+        reporter: 'spec'
+      },
+      all: { src: ['test/unit/*.js'] }
+    },
+    jshint: {
+      all: ['app.js', 'Gruntfile.js', 'models', 'routes', 'views/*.js', 'test'],
+      options: {
+        '-W030': true
+      }
+
+    },
+    casperjs: {
+      files: ['test/functional/**/*.js']
+    },
+    express: {
+      custom: {
+        options: {
+          port: 3000,
+          bases: 'www-root',
+          server: path.resolve('./app.js')
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // DEFAULT TASK
+  grunt.registerTask('default', ['jshint', 'simplemocha']);
+  grunt.registerTask('test', ['simplemocha']);
+
+};
