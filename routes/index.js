0760b762c5ffd29e6088796608d1796a2477cee1
Asif R Choudhury <choudhury.asif@gmail.com>
2014-12-25 03:09:05 -0600

    Add route for signup page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+
+router.get('/signup', function(req, res) {
+  res.render('signup', { title: "Sign Up" });
+});
+
+
 module.exports = router;
