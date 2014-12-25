739440ae9994c23ce3a8b74f7838fe4090855000
Asif R Choudhury <choudhury.asif@gmail.com>
2014-12-25 02:17:11 -0600

    Add /api/users route and make GET return list of all users

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+//Bismillah
+var router = require('express').Router();
+var mongoose = require('mongoose');
+
+//Connect to MongoDB
+var mongoUrl = "mongodb://localhost/buet73";
+mongoose.connect(mongoUrl);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function cb() {
+    console.log("Connected to MongoDB at "+mongoUrl); //DEV
+});
+
+var User = require('../models/User')(mongoose);
+
+var respond = function(res, result) {
+  res.contentType('application/json');
+  res.send(JSON.stringify(result));
+};
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  User.find({}, function(data) {
+    respond(res, data);
+  });
+});
+
+module.exports = router;
