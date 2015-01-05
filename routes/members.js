c5f4bd61e63e143f946370c4748198178f3cf219
Asif R Choudhury <choudhury.asif@gmail.com>
2015-01-05 21:07:43 +0600

    Reroute directory as /members

--- routes/members.js
+++ routes/members.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/directory', function(req, res) {
+router.get('/', function(req, res) {
   res.render('directory');
 });
 
